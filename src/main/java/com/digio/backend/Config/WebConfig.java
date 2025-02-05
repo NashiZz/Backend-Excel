43c9840575c434c3248d848cb7015e5395eaacbb
Nashi <115577029+NashiZz@users.noreply.github.com>
2025-02-05 12:55:42 +0700

    update-template

--- src/main/java/com/digio/backend/Config/WebConfig.java
+++ src/main/java/com/digio/backend/Config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://192.168.1.105:3000")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
